1b2964c3281040e2abc53f2a7c59507a314cebe7
AgentG5 <maksum3000@gmail.com>
2022-05-10 22:14:34 +0300

    v1

---
+++ src/com/company/Main.java
@@ -0,0 +1,97 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //zavdanja 1
+        // оператор  for
+        //for(int a = 500; a <= 650; a = a + 10) {
+        //System.out.print(a + " ");
+        //}
+
+        // оператор  while
+//        int a = 500;
+//        while (a <= 650) {
+//            System.out.println(a + " ");
+//            a = a + 10;
+//        }
+
+
+        //опператор  do-while
+//        int a = 500;
+//        do {
+//            System.out.println(a);
+//            a = a + 10;
+//        } while (a <= 650)
+
+
+        //zavdanja 2
+
+
+        //int a = 2;
+        //while (a <= 5000) {
+        // System.out.println(a + " ");
+        //  a = 2 * a - 1;
+        // }
+
+
+        //zavdanja 3
+
+
+     //   int i = 10;
+      //  for (int a = 1; a <= 10; a++) {
+        //    if (i % a == 0) {
+          //      System.out.println(a);
+         //   }
+      //  }
+
+
+
+        //zavdanja 4
+
+
+        // Метод for
+//        int result = 1;
+//        for(int i = 1; i <= 10; i = i +1){
+//            result = result * i;
+//            System.out.println(result);
+//
+//        }// Метод while
+//        int result = 1;
+//        int i = 1;
+//        while(i <= 10){
+//            result = result * i;
+//            System.out.println(result);
+//            i = i + 1;
+//        }
+
+
+
+
+        //zavdanja 5
+
+
+//        int odnakovi = 0;
+//        for (int firstn = 0; firstn <= 2; firstn++) {
+//            //String str1 = Integer.toString(n);
+//            for (int secondn = 0; secondn <= 9; secondn++) {
+//                //String str2 = Integer.toString(g);
+//                for(int thirdn = 0; thirdn < 6; thirdn++ ){
+//                    //String str3 = Integer.toString(t);
+//                    for(int fourthn = 0; fourthn <=9; fourthn++){
+//                        //String str4 = Integer.toString(u);
+//                        if(firstn == 2 && secondn <= 3){
+//                            continue;
+//                        }
+//                        if((firstn == thirdn && secondn == fourthn)||(firstn == fourthn && secondn == thirdn)){
+//                            odnakovi++;
+//                        }
+//                    }
+//                }
+//            }
+//        }System.out.println("Однакові " + odnakovi);
+    }
+}
+
+
+
